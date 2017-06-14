0a4b38187e2801be54db6d68bf94005869e509a1
x0156 <paradox@outlook.in>
2017-06-14 23:32:05 +0530

    cleanup

--- install.sh
+++ install.sh
@@ -2,30 +2,32 @@
 
 set -e
 
-CITS_FNAME=cognizant-intelligent-test-scripter
-CITS_VERSION=1.0-SNAPSHOT
-CITS_TAG=v1.0-snapshot
-CITS_REPO=CognizantQAHub/Cognizant-Intelligent-Test-Scripter
-CITS_ROOT=$HOME/lib/cits
-CITS_BIN=$HOME/bin/CITS
+NAME=cognizant-intelligent-test-scripter
+VERSION=1.0-SNAPSHOT
+TAG=v1.0-snapshot
+REPO=CognizantQAHub/Cognizant-Intelligent-Test-Scripter
+ROOT=$HOME/lib/cits
+BINARY=$HOME/bin/CITS
+
 #PATH=$CITS_ROOT:$PATH
 #download
-curl -O -L "https://github.com/$CITS_REPO/releases/download/$CITS_TAG/$CITS_FNAME-$CITS_VERSION-setup.zip"
+curl -O -L "https://github.com/$REPO/releases/download/$TAG/$NAME-$VERSION-setup.zip"
 #extract
-unzip $CITS_FNAME-$CITS_VERSION-setup.zip > unzip_log.txt
+unzip $NAME-$VERSION-setup.zip > unzip_log.txt
 #create install dir
-mkdir -p $CITS_ROOT
+mkdir -p $ROOT
 mkdir -p $HOME/bin
-mv $CITS_FNAME-$CITS_VERSION/* $CITS_ROOT
-cat $CITS_ROOT/Run.command > $CITS_ROOT/CITS
-echo "./$CITS_ROOT/Run.command $*" > $CITS_BIN
+mv $NAME-$VERSION/* $ROOT
+#cat $ROOT/Run.command > $ROOT/CITS
+#create binary
+echo "./$ROOT/Run.command $*" > $BINARY
 #apply permission
-chmod +x $CITS_BIN
-echo "$CITS_FNAME installed in $CITS_ROOT"
-echo "version:"
+chmod u+x $BINARY
+echo "$NAME from $REPO installed in $ROOT";echo "version:"
 CITS -version
+
 # creating sourcefile
-echo "export CITS_VERSION=$CITS_VERSION" > sourcefile
-echo "export CITS_TAG=$CITS_TAG" >> sourcefile
-echo "export CITS_ROOT=$CITS_ROOT" >> sourcefile
-#echo "export PATH=$CITS_ROOT:$PATH" >> sourcefile
+echo "export CITS_VERSION=$VERSION" > sourcefile
+echo "export CITS_TAG=$TAG" >> sourcefile
+echo "export CITS_ROOT=$ROOT" >> sourcefile
+echo "export PATH=$ROOT:$PATH" >> sourcefile
