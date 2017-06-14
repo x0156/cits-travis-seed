00065bb8178324e37ae891ca4eb64258848cdb45
Karuna <x0156@users.noreply.github.com>
2017-06-14 18:19:59 +0530

    [skip ci]

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -x
+set -e
 
 CITS_FNAME=cognizant-intelligent-test-scripter
 CITS_VERSION=1.0-SNAPSHOT
 CITS_TAG=v1.0-snapshot
 CITS_REPO=CognizantQAHub/Cognizant-Intelligent-Test-Scripter
-CITS_ROOT=lib/$CITS_FNAME/$CITS_VERSION
+export CITS_ROOT=lib/$CITS_FNAME/$CITS_VERSION
 #download
 curl -O -L "https://github.com/$CITS_REPO/releases/download/$CITS_TAG/$CITS_FNAME-$CITS_VERSION-setup.zip"
 #extract
