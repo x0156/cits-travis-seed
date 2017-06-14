cb163dde144173bfdc51308c25243718f2e78ee6
x0156 <paradox@outlook.in>
2017-06-14 23:02:20 +0530

    install in $HOME

--- install.sh
+++ install.sh
@@ -6,16 +6,18 @@ CITS_FNAME=cognizant-intelligent-test-scripter
 CITS_VERSION=1.0-SNAPSHOT
 CITS_TAG=v1.0-snapshot
 CITS_REPO=CognizantQAHub/Cognizant-Intelligent-Test-Scripter
-CITS_ROOT=lib/cits
-PATH=$CITS_ROOT:$PATH
+CITS_ROOT=$HOME/lib/cits
+CITS_BIN=$HOME/bin/CITS
+#PATH=$CITS_ROOT:$PATH
 #download
 curl -O -L "https://github.com/$CITS_REPO/releases/download/$CITS_TAG/$CITS_FNAME-$CITS_VERSION-setup.zip"
 #extract
-unzip $CITS_FNAME-$CITS_VERSION-setup.zip >> unzip_log.txt
+unzip $CITS_FNAME-$CITS_VERSION-setup.zip > unzip_log.txt
 #create install dir
 mkdir -p $CITS_ROOT
 mv $CITS_FNAME-$CITS_VERSION/* $CITS_ROOT
 cat $CITS_ROOT/Run.command > $CITS_ROOT/CITS
+echo "./$CITS_ROOT/Run.command $*" > $CITS_BIN
 #apply permission
 chmod +x $CITS_ROOT/CITS
 echo "$CITS_FNAME installed in $CITS_ROOT"
@@ -25,4 +27,4 @@ CITS -version
 echo "export CITS_VERSION=$CITS_VERSION" > sourcefile
 echo "export CITS_TAG=$CITS_TAG" >> sourcefile
 echo "export CITS_ROOT=$CITS_ROOT" >> sourcefile
-echo "export PATH=$CITS_ROOT:$PATH" >> sourcefile
+#echo "export PATH=$CITS_ROOT:$PATH" >> sourcefile
