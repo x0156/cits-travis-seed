0decda07de21faa6c6d1195f50fbf74a77a5a2d4
Karuna <paradox@outlook.in>
2017-07-19 18:08:46 +0530

    updated build script to support multiple stages

---
+++ scripts/ci/travis-scripts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Go to project/working directory
+cd $(dirname $0)/../..
+
+if [[ -z "$TRAVIS" ]]; then
+  echo "Not Travis!"
+  exit 1
+fi
+
+if [[ "${MODE}" ]]; then
+  ./scripts/ci/travis-testing.sh
+elif [[ "${DEPLOY_MODE}" ]]; then
+  echo "Nothing to  build!"
+fi
